ec546df1d43557e30696c7c15917a61dd227f1e0
Aman Kumar <aman@rudrainnovatives.com>
2018-08-02 10:59:26 +0530

    Scapper added

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,39 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+// 
+// .media-left { manga well p }
+// http://readcomicsonline.ru/comic/1985-black-hole-repo-2017
+// http://readcomicsonline.ru/comic-list
 
 Route::get('/', function() {
-    $crawler = Goutte::request('GET', 'https://www.amarujala.com/india-news');
-    $crawler->filter('.image-caption')->each(function ($node) {
-      dump($node->text());
+    $crawler = Goutte::request('GET', 'http://readcomicsonline.ru/comic/1985-black-hole-repo-2017');
+
+    // $crawler->filter('.chart-title ')->each(function ($node) {
+
+    //   // getting title 
+    //   print_r($node->text());
+    //   echo "<br>";
+    // });
+
+    // $crawler->filter('a > img')->each(function ($node) {
+
+    //   if($node->getNode(0)->tagName == 'img') {
+
+    //   	echo "<pre>";
+    //   	print_r($node->getNode(0)->getAttribute('src'));
+    //   }
+
+    // });
+
+    $crawler->filter('p')->each(function ($node) {
+
+      echo "<pre>";
+      print_r($node->text());
+
     });
+
+    die("here");
 
     return view('welcome');
 });
